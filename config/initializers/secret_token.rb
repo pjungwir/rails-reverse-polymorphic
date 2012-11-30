957c1cb3349d8184f77b319f4b0e5700f29a90ab
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2012-11-30 13:10:31 -0800

    Empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsReversePolymorphic::Application.config.secret_token = '21edb5c5dee2557e31761ee0db6718a2b88491fa2dee5926f1115f576e78c732e6fdd0c4784c477761df5e2109b25033cde008b12c77d35a12db1a9c7d83ebc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsReversePolymorphic::Application.config.session_store :cookie_store, key: '_rails-reverse-polymorphic_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsReversePolymorphic::Application.config.session_store :active_record_store
